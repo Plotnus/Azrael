71d0b81d0d83c62c80f9c2fb32d32c6e351a7e7d
Jon Plotner <plotnus@gmail.com>
2016-09-14 18:02:19 -0700

    Create Unreal Project

---
+++ UProject/Source/Azrael/AzraelGameMode.h
@@ -0,0 +1,18 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "AzraelGameMode.generated.h"
+
+
+UCLASS(minimalapi)
+class AAzraelGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AAzraelGameMode();
+};
+
+
+
